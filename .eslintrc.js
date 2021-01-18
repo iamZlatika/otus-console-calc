52139d9520f23447e37c79453663468c4391694c
iamzlatika <mazurovazv@gmail.com>
2021-01-18 20:21:45 +0300

    Configure project environment

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "plugin:jest/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "jest"],
+  rules: {},
+};
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# otus-console-calc
+# Otus Console Calculator
+
+TBD
